ed05c615604caecd49e990c00619da2817803969
Gabrielle Monteiro <gabrielle.monteiro95@outlook.com>
2021-06-23 21:13:21 -0300

    Criação de controllers, routes, repositories, services, entities

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from 'express'
+import { router } from "./routes";
 
 import "./database";
 
@@ -7,7 +8,7 @@ const app = express();
 const port = 8080 
 
 app.use(express.json());
-
+app.use(router);
 
 
 app.listen(port);
